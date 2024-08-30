3610672a6407ffbacc1cf74713f641395a42eb2c
Caelan Desmond <ccdesmon@sundevils.asu.edu>
2024-08-30 01:59:54 -0700

    Add project files.

---
+++ ConsoleCalculator/Basic4Fun.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+
+public class Basic4Fun
+{
+	//Constructor
+	public Basic4Fun()
+	{
+        History = new List<String>();
+		Ihistory = 0;
+    }
+	public string ToString()
+	{
+		return "4 Function Calculator";
+	}
+	//History
+	List<String> History;
+	int Ihistory;
+	public void ClearHistory()
+	{
+		History.Clear();
+	}
+	/// <summary>
+	/// Reads the Last entry, Multiple uses will iterate backwards
+	/// </summary>
+	/// <returns>"{a} {operator} {b} = {result} \n"</returns>
+	public string ReadRecentHistory()
+	{
+		if (History.Count == Ihistory)
+		{
+			return "No Futher History";
+		}
+		string history = History[History.Count - Ihistory - 1];
+        Ihistory++;
+        return history;
+	}
+    /// <summary>
+    /// Reads all history from youngest to oldest
+    /// </summary>
+    /// <returns>"{a} {operator} {b} = {result} \n" {next entry} ...</returns>
+    public string ReadAllHistory()
+	{
+		Ihistory = 0;
+		string result = "";
+		foreach (string entry in History)
+		{
+			result = entry + "\n" + result;
+		}
+		result = result + "End of History";
+		return result;
+	}
+
+	//Integer 5 function
+	public int Add(int a, int b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " + " + b.ToString() + " = " + (a + b).ToString());
+		return a + b;
+	}
+	public int Subtract(int a, int b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " - " + b.ToString() + " = " + (a + b).ToString());
+        return a - b;
+	}
+	public int Multiply(int a, int b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " * " + b.ToString() + " = " + (a + b).ToString());
+        return a * b;
+	}
+	public int Divide(int a, int b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " / " + b.ToString() + " = " + (a + b).ToString());
+        return a / b;
+	}
+	public int Mod(int a, int b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " % " + b.ToString() + " = " + (a + b).ToString());
+        return a % b;
+	}
+	
+	//Double 4 function
+	public double Add(double a, double b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " + " + b.ToString() + " = " + (a + b).ToString());
+        return a + b;
+	}
+	public double Subtract(double a, double b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " - " + b.ToString() + " = " + (a - b).ToString());
+        return a - b;
+	}
+	public double Multiply(double a, double b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " * " + b.ToString() + " = " + (a * b).ToString());
+        return a * b;
+	}
+	public double Divide(double a, double b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " / " + b.ToString() + " = " + (a / b).ToString());
+        return a / b;
+	}
+
+	//float 4 function
+	public float Add(float a, float b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " + " + b.ToString() + " = " + (a + b).ToString());
+        return a + b;
+	}
+	public float Subtract(float a, float b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " - " + b.ToString() + " = " + (a - b).ToString());
+        return a - b;
+	}
+	public float Multiply(float a, float b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " * " + b.ToString() + " = " + (a * b).ToString());
+        return a * b;
+	}
+	public float Divide(float a, float b)
+	{
+        Ihistory = 0;
+        History.Add(a.ToString() + " / " + b.ToString() + " = " + (a / b).ToString());
+        return a / b;
+	}
+
+	//string 5 function
+	//Impebedding convertion to doubles
+	public double Add(string aS, string bS)
+	{
+		Ihistory = 0;
+		double a = Double.Parse(aS);
+		double b = Double.Parse(bS);
+        History.Add(a.ToString() + " + " + b.ToString() + " = " + (a + b).ToString());
+        return a + b;
+	}
+    public double Subtract(string aS, string bS)
+    {
+        Ihistory = 0;
+        double a = Double.Parse(aS);
+        double b = Double.Parse(bS);
+        History.Add(a.ToString() + " - " + b.ToString() + " = " + (a - b).ToString());
+        return a - b;
+    }
+    public double Multiply(string aS, string bS)
+    {
+        Ihistory = 0;
+        double a = Double.Parse(aS);
+        double b = Double.Parse(bS);
+        History.Add(a.ToString() + " * " + b.ToString() + " = " + (a * b).ToString());
+        return a * b;
+    }
+    public double Divide(string aS, string bS)
+    {
+        Ihistory = 0;
+        double a = Double.Parse(aS);
+        double b = Double.Parse(bS);
+        History.Add(a.ToString() + " / " + b.ToString() + " = " + (a / b).ToString());
+        return (a / b);
+    }
+	public int? Mod (string aS, string bS)
+	{
+		Ihistory = 0;
+		int a;
+		int b;
+		bool status = int.TryParse(aS, out a);
+		if (status)
+		{
+            status = int.TryParse(bS, out b);
+        }
+		else
+		{
+			return null;
+		}
+		if (status)
+		{
+            History.Add(a.ToString() + " % " + b.ToString() + " = " + (a % b).ToString());
+            return a % b;
+        }
+		return null;
+	}
+}
---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,101 @@
+﻿Basic4Fun basic4Fun = new Basic4Fun();
+Console.WriteLine("Welcome to the Basic4fun interaction interface");
+Console.WriteLine("To use please type a 2 operand equation or type Help for more options");
+bool end = false;
+do
+{
+    string input = Console.ReadLine();
+    if (input == null || input == "")
+    {
+    }
+    else if (input == "help")
+    {
+        Console.WriteLine("To use please input your Equation in the following format: {a} {operator} {b}");
+        Console.WriteLine("ReadRecentHistory - Read the lastest History, Repeated use will backtrack further");
+        Console.WriteLine("ReadAllHistory - Read All History from youngest to oldest");
+        Console.WriteLine("Quit - End the program");
+    }
+    else if (input == "quit")
+    {
+        end = true;
+    }
+    else if (input == "ReadRecentHistory")
+    {
+        Console.WriteLine(basic4Fun.ReadRecentHistory());
+    }
+    else if (input == "ReadAllHistory")
+    {
+        Console.WriteLine(basic4Fun.ReadAllHistory());
+    }
+    else if (Char.IsDigit(input[0]))
+    {
+        string[] parts = input.Split(' ');
+        if (parts.Length != 3 && parts.Length != 1)
+        {
+            Console.WriteLine("Error: Improper Syntax");
+        }
+        else if (parts.Length == 3)
+        {
+            string a = parts[0];
+            string op = parts[1];
+            string b = parts[2];
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(basic4Fun.Add(Double.Parse(a), Double.Parse(b)));
+                    break;
+                case "-":
+                    Console.WriteLine(basic4Fun.Subtract(Double.Parse(a), Double.Parse(b)));
+                    break;
+                case "*":
+                    Console.WriteLine(basic4Fun.Multiply(Double.Parse(a), Double.Parse(b)));
+                    break;
+                case "/":
+                    Console.WriteLine(basic4Fun.Divide(Double.Parse(a), Double.Parse(b)));
+                    break;
+                case "%":
+                    Console.WriteLine(basic4Fun.Mod(int.Parse(a), int.Parse(b)));
+                    break;
+                default:
+                    Console.WriteLine("Error: Improper Syntax");
+                    break;
+            }
+        }
+        else
+        {
+            if(input.Split('+').Length == 2)
+            {
+                parts = input.Split("+");
+                Console.WriteLine(basic4Fun.Add(parts[0], parts[1]));
+            }
+            else if(input.Split("-").Length == 2)
+            {
+                parts = input.Split("-");
+                Console.WriteLine(basic4Fun.Subtract(parts[0], parts[1]));
+            }
+            else if (input.Split("*").Length == 2)
+            {
+                parts = input.Split("*");
+                Console.WriteLine(basic4Fun.Multiply(parts[0], parts[1]));
+            }
+            else if (input.Split("/").Length == 2)
+            {
+                parts = input.Split("/");
+                Console.WriteLine(basic4Fun.Divide(parts[0], parts[1]));
+            }
+            else if (input.Split("%").Length == 2)
+            {
+                parts = input.Split("%");
+                Console.WriteLine(basic4Fun.Mod(parts[0], parts[1]));
+            }
+            else
+            {
+                Console.WriteLine("Error: Improper Syntax");
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Error: Improper Syntax");
+    }
+}while (!end);
