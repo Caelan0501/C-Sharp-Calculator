2eede303af6259d8bcee4aec2b323e4d1495a6c2
Caelan Desmond <caelan0501@gmail.com>
2024-09-19 02:28:44 -0700

    Migrated all classes to a new Class Library project for future expansion of the project to include diffrent .Net features without redundancy problems.

---
+++ Calculator/Basic6Fun.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Calculator
+{
+    public class Basic6Fun : Basic4Fun
+    {
+        public Basic6Fun(bool enabledHistory = true) : base(enabledHistory: enabledHistory)
+        {
+        }
+        public override string ToString()
+        {
+            return "6 Function Calculator";
+        }
+        public int Power(int a, int b)
+        {
+            AddToHistory(a, '^', b, (int)(Math.Pow((double)(a), (double)(b))));
+            return (int)(Math.Pow((double)(a), (double)(b)));
+        }
+        public double Power(double a, double b)
+        {
+            AddToHistory(a, '^', b, Math.Pow(a, b));
+            return Math.Pow(a, b);
+        }
+        public float Power(float a, float b)
+        {
+            AddToHistory(a, '^', b, (float)Math.Pow(a, b));
+            return (float)Math.Pow(a, b);
+        }
+        public double Power(string aS, string bS)
+        {
+            double a = Double.Parse(aS);
+            double b = Double.Parse(bS);
+            AddToHistory(a, '^', b, Math.Pow(a, b));
+            return Math.Pow(a, b);
+        }
+
+        public double Root(double a, double b)
+        {
+            AddToHistory(a, '√', b, Power(a, Divide(1, b)));
+            return Power(a, Divide(1, b));
+        }
+        public int Root(int a, int b)
+        {
+            AddToHistory(a, '√', b, Power(a, Divide(1, b)));
+            return Power(a, Divide(1, b));
+        }
+        public float Root(float a, float b)
+        {
+            AddToHistory(a, '√', b, Power(a, Divide(1, b)));
+            return Power(a, Divide(1, b));
+        }
+        public double Root(string aS, string bS)
+        {
+            double a = Double.Parse(aS);
+            double b = Double.Parse(bS);
+            history.Pause();
+            double result = Power(a, Divide(1, b));
+            history.Resume();
+            AddToHistory(a, '√', b, result);
+            return result;
+        }
+    }
+
+}
---
+++ Calculator/History.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    public class History
+    {
+        private List<string>? history;
+        private bool paused;
+        private bool isChanged;
+
+        public History(bool enabled = true)
+        {
+            paused = false;
+            isChanged = true;
+            if (enabled)
+            {
+                history = new List<string>();
+            }
+        }
+        public void Clear()
+        {
+            if (history != null) history.Clear();
+        }
+        public void Resume()
+        {
+            paused = false;
+        }
+        public void Pause()
+        {
+            paused = true;
+        }
+        public bool IsEnabled()
+        {
+            return history == null;
+        }
+        public void AddEntry(string entry)
+        {
+            if (!(history == null) && !paused)
+            {
+                history.Add(entry);
+                isChanged = true;
+            }
+        }
+
+        /// <summary>
+        /// Reads all history from youngest to oldest
+        /// </summary>
+        /// <returns>"{a} {operator} {b} = {result} \n" {next entry} ...</returns>
+        public string ReadAll()
+        {
+            if (history == null) return "";
+            string result = "";
+            foreach (string entry in history)
+            {
+                result = entry + "\n" + result;
+            }
+            result = result + "End of History";
+            return result;
+        }
+
+        int recentcyIndex = 0;
+        public string ReadRecent()
+        {
+            if (history == null) return "";
+            if (isChanged == true)
+            {
+                recentcyIndex = 0;
+                isChanged = false;
+            }
+            if (recentcyIndex != history.Count)
+            {
+                string result = history[history.Count - recentcyIndex - 1];
+                recentcyIndex++;
+                return result;
+            }
+            return "No Further History";
+        }
+    }
+
+
+}
